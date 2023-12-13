dd6eb3d8e48f3b89701ba81e6917d109dabf0a1f
brando2332 <brandosteven2003@gmail.com>
2023-12-13 17:25:23 -0400

    hola

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
 axios.get("https://leonardoapi.onrender.com/music")
     .then((res) => {
-        console.log(res.data)
+        res.data.map((song) => {
+            
+            let div = document.createElement("div")
+            div.classList.add("card-concentracion")
+
+            div.innerHTML = `
+            <div class="card">
+            <div class="card-img">
+                <img
+                    src="${song.path.front}"
+                    alt="${song.album} - ${song.title}"
+                />
+            </div>
+            <h2>${song.title}</h2>
+            <p>${song.album} - ${song.author}</p>
+            </div>
+            `
+            
+            div.addEventListener("click" ,  () => {
+                
+                document.querySelector("#current-song-title").innerHTML = song.title
+
+                document.querySelector("#current-song-author").innerHTML = song.author
+
+                document.querySelector("#current-song-img").setAttribute("src", song.path.front)
+
+                document.querySelector("#current-song-audio").setAttribute("src", song.path.audio) 
+
+            })
+
+
+
+
+            document.querySelector("#tracklist").
+            appendChild(div)
+
+
+
+
+
+
+
+
+
+
+
     })
+})
